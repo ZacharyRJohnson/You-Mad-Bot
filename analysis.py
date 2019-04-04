43d21e7b66c52cff44045deba210515d64145db3
ZacharyRJohnson <zachjohnson1900@gmail.com>
2019-04-04 14:17:15 -0400

    Created .gitignore

--- analysis.py
+++ analysis.py
@@ -3,18 +3,17 @@ from google.cloud.language import enums
 from google.cloud.language import types
 
 def analyze(message):
-    document = types.Document(content = message, type = enum.Document.Type.PLAIN_TEXT)
+    document = types.Document(content = message, type = enums.Document.Type.PLAIN_TEXT)
     sentiment = client.analyze_sentiment(document=document).document_sentiment
     return sentiment
+
+
 
 client = language.LanguageServiceClient()
 
 text = 'Fuck! I\'m so pissed. That movie was so bad I can\'t believe I spent money on that.'
-document = types.Document(
-    content = text,
-    type = enums.Document.Type.PLAIN_TEXT)
 
-sentiment = client.analyze_sentiment(document=document).document_sentiment
+sentiment = analyze(text)
 
 print('Text: {}'.format(text))
 print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
