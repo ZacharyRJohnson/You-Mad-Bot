35cf07d1d39e41fa3784ebb59a9a6a1dd77452ca
ZacharyRJohnson <zachjohnson1900@gmail.com>
2019-04-09 01:22:18 -0400

    Added new function to analysis to calculate average sentiment info from a collection of messages

--- analysis.py
+++ analysis.py
@@ -1,28 +1,43 @@
-import io
-import json
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-from google.oauth2 import service_account
+
+class Sentiment():
+    """
+        Simple class to hold data for calculated sentiment information from a collection
+        of messages. Data such as average score/magnitude, max/min score from a collection, etc.
+    """
+
+    def __init__(self, avg_score, avg_mag):
+        self.avg_score = avg_score
+        self.avg_magnitude = avg_mag
+
 
 def analyze(message, client):
     document = types.Document(content = message, type = enums.Document.Type.PLAIN_TEXT)
     sentiment = client.analyze_sentiment(document=document).document_sentiment
-    print(str(sentiment.score))
     return sentiment
-"""
-# Need your own private GCP service account key for the api calls
-with open('apikey.json', 'r') as key_file:
-    key = key_file.read()
-    key_json = json.loads(key, strict=False)
-    creds = service_account.Credentials.from_service_account_info(key_json)
 
-client = language.LanguageServiceClient(credentials=creds)
+def get_average_sentiment(messages, client):
+    avg_score = 0
+    avg_magnitude = 0
+    counted_msgs = 0
+    for msg in messages:
+        sentiment = analyze(msg, client)
+        score = sentiment.score
+        magnitude = sentiment.magnitude
+        
+        '''
+        Rarely does a document equal zero. More than likely the analysis couldn't accurately 
+        be calculated for some reason so those messages will be left out of the average.
+        '''
+        if(score == 0.0):
+            continue
+        else:
+            avg_score += score
+            avg_magnitude += magnitude
+            counted_msgs += 1
+    avg_score = avg_score / counted_msgs
+    avg_magnitude = avg_magnitude / counted_msgs
 
-text = 'Fuck! I\'m so pissed. That movie was so bad I can\'t believe I spent money on that.'
-
-sentiment = analyze(text)
-
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-"""
+    return Sentiment(avg_score, avg_magnitude)
